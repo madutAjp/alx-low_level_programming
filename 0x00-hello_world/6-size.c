637e1502d78844b31bb2931144d59625a5dca8b5
madutAjp <maduthuolin@gmail.com>
2023-09-07 01:30:34 -0700

    c hello wolrd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	  printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of char: %zu byte\n", sizeof(char));
+
+    return 0;
+}
