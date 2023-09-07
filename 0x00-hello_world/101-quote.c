abb3b68c9291703a4e48a526343927f450b5a4b8
madutAjp <maduthuolin@gmail.com>
2023-09-07 06:32:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <unistd.h>
-
 
 /**
  * main - write the string standard output
@@ -11,6 +9,6 @@
 
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
