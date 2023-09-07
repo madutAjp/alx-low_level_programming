5153e631785bd415089d1a3d981b1b0384e7ff27
madutAjp <maduthuolin@gmail.com>
2023-09-07 03:17:47 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+
+
 /**
  * main - write the string output
  *
@@ -7,6 +9,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but outcome is a pieace of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
