86d1a9a2a26300ab66faf19dac24207c22edf976
madutAjp <maduthuolin@gmail.com>
2023-09-07 04:37:05 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
+
+
 /**
- * main - Entry point
+ * main - write a string to the standard output
  *
- * Return: Always 0 (success)
+ * Return: 0 when successfull
  */
+
 int main(void)
 {
-	  printf("Size of int: %zu bytes\n", sizeof(int));
-    printf("Size of float: %zu bytes\n", sizeof(float));
-    printf("Size of double: %zu bytes\n", sizeof(double));
-    printf("Size of char: %zu byte\n", sizeof(char));
-
-    return 0;
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
 }
