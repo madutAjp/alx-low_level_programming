53ad4b18bd8ec05d5590e138ab69e87276538a75
madutAjp <maduthuolin@gmail.com>
2023-09-08 15:25:29 -0700

    smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Start here
+ *
+ * Return: 0 when done
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'z' ; a >= 'a' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
