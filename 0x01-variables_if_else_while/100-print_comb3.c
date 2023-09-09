0e7236f4f5b901fd6405ec007790071924fc5afe
madutAjp <maduthuolin@gmail.com>
2023-09-09 10:43:21 -0700

    inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int m, p;
 
-	for (m = 0; m <= 8; m++)
+	for (m = 10; m <= 18; m++)
 	{
 		putchar(m + '0');
 	}
-	for (p = m + 1; p <= 9; p++)
+	for (p = m + 11; p <= 29; p++)
 	{
 		putchar(p + '0');
 	}
-	if (m < 8 || p < 9)
+	if (m < 18 || p < 29)
 	{
 		putchar(',');
 		putchar(' ');
