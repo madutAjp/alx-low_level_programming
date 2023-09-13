0267721b7e56a43aeb83ac3e9c7fc1b8d23693cf
madutAjp <maduthuolin@gmail.com>
2023-09-13 08:54:03 -0700

    street battery

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,10 +20,10 @@ void print_to_98(int n)
 	}
 	printf("\n");
 	}
-	}
-	else if (n > 98)
 	{
-	for (i = n; i >= 98; i--)
+	if (n < 0)
+	{
+	for (i = n; i >= 98; i++)
 	{
 	printf("%d", i);
 	if (i != 98)
@@ -31,6 +31,16 @@ void print_to_98(int n)
 	}
 	printf("\n");
 	}
-	else if (n == 98)
+	else if (n > 98)
+	{
+	for (i = n; i >= 98; i--)
+	{
+	printf("%d", i);
+	}
 	printf("\n");
+	}
+	else if (n == 98)
+	printf("%d\n", n);
+	}
+	}
 }
