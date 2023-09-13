38d10c0fcf54ede88bddabd98aa71b3ea524635f
madutAjp <maduthuolin@gmail.com>
2023-09-13 14:28:49 -0700

    natural naumbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,13 +11,13 @@ int main(void)
 	int sum = 0;
 	int m;
 
-	for (m = 3; m < limit; m++)
+	for (m = 1; m < 1024; m++)
 	{
 	if (m % 3 == 0 || m % 5 == 0)
 	{
-	sum += m;
+	sum = sum + m;
 	}
 	}
-	_putchar(sum);
+	printf("%d\n", sum);
 	return (0);
 }
