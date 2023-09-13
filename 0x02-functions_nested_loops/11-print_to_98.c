4d0c0e5687d7bc71756c39595d28eb0a9fbfd9b5
madutAjp <maduthuolin@gmail.com>
2023-09-13 07:42:42 -0700

    battery street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - To prints numbers from n to 98
+ * @n:number to start printing from
+ * Return:0 when when successful
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+	for (i = n; i <= 98; i++)
+	{
+	printf("%d", i);
+	if (i != 98)
+	{
+	printf(",");
+	}
+	printf("\n");
+	}
+	}
+	else if (n > 98)
+	{
+	for (i = n; i >= 98; i--)
+	{
+	printf("%d", i);
+	if (i != 98)
+	printf(",");
+	}
+	printf("\n");
+	}
+	else if (n == 98)
+	printf("\n");
+}
