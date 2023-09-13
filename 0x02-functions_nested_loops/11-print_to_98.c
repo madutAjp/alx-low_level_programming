5a66c20c157c1e2471258a17cf46c748e7382cc0
madutAjp <maduthuolin@gmail.com>
2023-09-13 10:00:58 -0700

    battery street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 0 && n <= 98)
+	if (n <= 98)
 	{
 	for (i = n; i <= 98; i++)
 	{
@@ -18,29 +18,18 @@ void print_to_98(int n)
 	{
 	printf(",");
 	}
-	printf("\n");
 	}
-	{
-	if (n < 0)
-	{
-	for (i = n; i >= 98; i++)
-	{
-	printf("%d", i);
-	if (i != 98)
-	printf(",");
 	}
-	printf("\n");
-	}
-	else if (n > 98)
+	else
 	{
 	for (i = n; i >= 98; i--)
 	{
 	printf("%d", i);
+	if (i != 98)
+	{
+	printf(",");
+	}
+	}
 	}
 	printf("\n");
-	}
-	else if (n == 98)
-	printf("%d\n", n);
-	}
-	}
 }
