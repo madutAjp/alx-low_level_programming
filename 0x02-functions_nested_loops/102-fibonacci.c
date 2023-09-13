289fa64e645adbf977720084a02244aa18f7069c
madutAjp <maduthuolin@gmail.com>
2023-09-13 15:18:40 -0700

    computer class

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - program prints first 50 fibonacci numbers
+ *
+ * Return:0 when successful
+ */
+int main(void)
+{
+	unsigned long int  t1 = 1;
+	unsigned long int t2 = 2;
+	unsigned long int next;
+	int k;
+
+	printf("%lu", t1);
+	for (k = 1 ; k < 50 ; k++)
+	{
+	printf("%ld", t2);
+	next = t1 + t2;
+	t1 = t2;
+	t2 = t1;
+	if (k != 49)
+	{
+	printf(",");
+	}
+	}
+	printf("\n");
+	return (0);
+}
