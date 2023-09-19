e9a94bcf898ac284a7988bb2e1a6a66d1a49be23
madutAjp <maduthuolin@gmail.com>
2023-09-19 13:05:02 -0700

    great leaders sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -27,7 +27,8 @@ int _atoi(char *s)
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-	if (result > (INT_MAX - (s[i] - '0')) / 10)
+	if (result > (INT_MAX / 10) ||
+		(result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
 	{
 	if (sign == 1)
 	return (INT_MAX);
