274b2f208873b505044c538aed0fed45766a5e01
madutAjp <maduthuolin@gmail.com>
2023-09-20 14:47:28 -0700

    my name

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,20 @@
 
 /**
  * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string to append to dest.
- * Return: A pointer to the resulting string dest.
+ * @dest:A pointer to the destination string.
+ * @src:A pointer to the source string to append to dest.
+ * Return:A pointer to the resulting string dest.
  */
 char *_strcat(char *dest, char *src)
 {
 	int m;
 	int v;
 
-	m = 0;
 	while (dest[m] != '\0')
 	{
 		m++;
 	}
-	v = 0;
+
 	while (src[v] != '0')
 	{
 		dest[m] = src[v];
