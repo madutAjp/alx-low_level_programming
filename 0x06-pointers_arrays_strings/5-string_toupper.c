109d57d0e99bb7ba17032a342aa62d6e7d8ce256
madutAjp <maduthuolin@gmail.com>
2023-09-20 16:18:18 -0700

    string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * string_touper - To coverts all lowercase letters
+ * @str:The input string to be modified
+ *
+ * Return:A pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+	if (islower(str[i]))
+	{
+	str[i] = toupper(str[i]);
+	}
+	i++;
+	}
+	return (str);
+}
