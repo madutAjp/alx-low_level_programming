79857b11178dea064c70b08e385c7157326168d7
madutAjp <maduthuolin@gmail.com>
2023-09-25 05:31:05 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,13 @@ char *_strchr(char *s, char c)
 {
 	int m;
 
-	for (m = 0; s[m] != '\0'; m++)
+	for (m = 0; s[m] >= '\0'; m++)
 	{
-	if (s[m] == c)
+		if (s[m] == c)
 	{
-	return (s + m);
+			return (&s[m]);
 	}
 	s++;
 	}
-	return ('\0');
+	return (NULL);
 }
