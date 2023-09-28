89df79230e22c087026b624afc2e2bd961848006
madutAjp <maduthuolin@gmail.com>
2023-09-28 05:29:24 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,30 +2,41 @@
 #include <stdio.h>
 
 /**
- * is_prime_number - Checks if an integer is a prime number
+ * is_divisible - Helper function to check if a number is divisible by another
+ * @n: The number to be checked for divisibility
+ * @divisor: The divisor to check against
+ * Return: 1 if n is divisible by divisor, 0 otherwise
+ */
+int is_divisible(int n, int divisor)
+{
+	if (divisor == 1)
+	{
+		return (0);
+	}
+	if (divisor == n)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (1);
+	}
+	return (is_divisible(n, divisor + 1));
+}
+/**
+ * is_prime_number - Checks if an integer is a prime number recursively
  * @n: The integer to be checked
  * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	if (n <= 3)
+	if (n == 2)
 	{
-	return (1);
+		return (1);
 	}
-	if (n % 2 == 0 || n % 3 == 0)
-	{
-		return (0);
-	for (i = 5; i * i <= n; i += 6)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
-	}
-	}
-	return (1);
+	return (!is_divisible(n, 2));
 }
