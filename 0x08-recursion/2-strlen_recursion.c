5081972fa6e30b39032d8a1d4bea546db89ad8d3
madutAjp <maduthuolin@gmail.com>
2023-09-28 03:31:50 -0700

    something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - A function that returns the length of a string
+ * @s:The string for which to find the length
+ * Return:The length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
