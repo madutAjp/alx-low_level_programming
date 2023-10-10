435fecc6987569fd2b0978c673646c5706967dfa
madutAjp <maduthuolin@gmail.com>
2023-10-10 11:21:53 -0700

    uncondition love

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,18 +7,12 @@
  * @owner:The owner of the dog
  * description:A struct with dog's information
  */
-/**
- * dog_t - new name for struct dog
- */
-
-typedef struct dog dog_t;
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
