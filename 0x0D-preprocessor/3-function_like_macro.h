578b88a9b55d8852106a8f39e76227ee65945362
madutAjp <maduthuolin@gmail.com>
2023-10-10 04:08:05 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef MARCOLIKE_H
+#define MARCOLIKE_H
+/**
+ * file - 3-function_like_macro.h
+ * @x:The absolute value of number to be computed
+ * description:The header file that computes the absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
