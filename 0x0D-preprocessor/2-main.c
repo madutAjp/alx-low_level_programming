3b4c104ed17cdde8e57ef4bf1cb9984e69a14ec3
madutAjp <maduthuolin@gmail.com>
2023-10-10 03:35:36 -0700

    file name it was compile

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the name of the file it was compiled from
+ *
+ * Return:int
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return(0);
+}
