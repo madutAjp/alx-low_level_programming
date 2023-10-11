c5984f914e6a03fb31313108d1988c3590183d6e
madutAjp <maduthuolin@gmail.com>
2023-10-11 16:17:01 -0700

    hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,23 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-/**
- * print_opcodes - TO prints opcodes of the program
- * @y:Adress of the main function
- * @n:Number of bytes to print
- * Return:void
- */
-void print_opcodes(char *y, int n)
-{
-	int x;
 
-	for (x = 0; x < n; x++)
-	{
-		printf("%.2hhx", y[x]);
-		if (x < n - 1)
-			printf(" ");
-	}
-	printf("\n");
-}
 /**
  * main - To prints its own opcodes
  * @argc:Number of arguments
@@ -26,22 +9,30 @@ void print_opcodes(char *y, int n)
  */
 int main(int argc, char *argv[])
 {
-	int j;
+	int i;
 	int bytes;
-	char *main_address = (char *)main;
+	char *arr;
 
 	if (argc != 2)
 	{
 		printf("error\n");
-		return (1);
+		exit(1);
 	}
 	bytes = atoi(argv[1]);
 	if (bytes < 0)
 	{
 		printf("error\n");
-		return (2);
+		exit(2);
 	}
-	main_address = (char *)main;
-	print_opcodes(main_address, bytes);
+	arr = (char *)main;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx", arr[i]);
+	}
 	return (0);
 }
