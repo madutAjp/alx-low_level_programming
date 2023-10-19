e4bb54cd5e3338fd4d0916a8e89394d2e2d0a515
madutAjp <maduthuolin@gmail.com>
2023-10-19 08:48:35 -0700

    free

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,11 @@ void first(void)__attribute__((constructor));
 /**
  * first - prints sentence before main
  * function is executed
+ * Return:void
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
+	return;
 }
