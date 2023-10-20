aba9cefcff51ae7f35265f4a9b15ecb3f3b3e716
madutAjp <maduthuolin@gmail.com>
2023-10-19 18:11:01 -0700

    my print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,10 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
+		if (h->next)
 		h = h->next;
+		else
+			break;
 		nodeCount++;
 	}
 	return (nodeCount);
