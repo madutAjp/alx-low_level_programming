3972169391f0e29c0ab11148d673708b52603fcf
madutAjp <maduthuolin@gmail.com>
2023-10-23 15:56:34 -0700

    delete my index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *irene, *ajp;
-	unsigned int madut;
+	unsigned int madut = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
