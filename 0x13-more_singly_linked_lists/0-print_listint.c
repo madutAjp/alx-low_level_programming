fe4bc5050301aa538f2236f6bbf15ad87e5edded
madutAjp <maduthuolin@gmail.com>
2023-10-23 11:37:00 -0700

    my print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  * print_listint - prints all the elements of a listint_t list
@@ -10,13 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t madut = 0;
+	size_t n = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		madut++;
+		n++;
 	}
-	return (madut);
+	return (n);
 }
