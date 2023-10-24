0dbff638118503e9166bfa375591d8227c702315
madutAjp <maduthuolin@gmail.com>
2023-10-24 14:29:19 -0700

    reversed 2

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds the loop in a linked list
+ * @head:pointer to the head of the list
+ * Return:node where the loop starts, or NULL if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortoise = head;
+	listint_t *hare = head;
+
+	if (head == NULL)
+		return (NULL);
+	while (hare != NULL && hare->next != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+	}
+	return (NULL);
+}
+
