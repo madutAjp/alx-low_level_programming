7b564dfc342a773b14bba6ba44f958d83f9ad824
madutAjp <maduthuolin@gmail.com>
2023-10-24 14:37:36 -0700

    freeland

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,31 +1,30 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - frees listint_t list
- * @h:pointer to the head of the list
- * Return: size of the list that was freed
+ * free_listint_safe - Frees a listint_t list
+ * @h: A pointer to a pointer to the head of the list
+ * Return: The size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *irene;
-	listint_t *ajp;
-	size_t madut = 0;
+	size_t count = 0;
+	listint_t *current, *temp;
 
-	irene = *h;
-	while (irene != NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+	current = *h;
+	while (current != NULL)
 	{
-		ajp = irene;
-		irene = irene->next;
-		free(ajp);
-		madut++;
-		if (ajp == *h)
+		temp = current;
+		current = current->next;
+		free(temp);
+		count++;
+		if (temp == *h)
 		{
 			*h = NULL;
 			break;
 		}
 	}
-	return (madut);
+	return (count);
 }
