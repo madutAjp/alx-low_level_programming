554ca0640559edb2341112c709c5702318e02c59
madutAjp <maduthuolin@gmail.com>
2023-10-26 07:34:20 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of a bit to 1 at a specific index
+ * @n: A pointer to the number to modify
+ * @index: The index of the bit to set to 1 (starting from 0)
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int madut;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	madut = 1UL << index;
+	*n |= madut;
+	return (1);
+}
